37a03e9e4b1f5b5fec2b01b8fef837638e454cf2
Catchem87 <moras.ashley87@gmail.com>
2018-11-23 12:44:25 -0500

    results js

---
+++ assets/javascript/results.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    var flightResults =  JSON.parse(localStorage.getItem('flightResults'));
+
+    var cityResults = JSON.parse(localStorage.getItem('cityResults'));
+
+    var i;
+    for (i = 0; i < cityResults.length; i++){
+        $()
+    }
+
+    
+    console.log(flightResults)
+})
