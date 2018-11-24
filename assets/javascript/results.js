ee3326b2438998bb80991d34de05ae5115b67662
Catchem87 <moras.ashley87@gmail.com>
2018-11-24 10:36:15 -0500

    console.log parsed result data

--- assets/javascript/results.js
+++ assets/javascript/results.js
@@ -1,13 +1,62 @@
 $(document).ready(function(){
     var flightResults =  JSON.parse(localStorage.getItem('flightResults'));
-
-    var cityResults = JSON.parse(localStorage.getItem('cityResults'));
-
-    var i;
-    for (i = 0; i < cityResults.length; i++){
-        $()
-    }
-
-    
     console.log(flightResults)
+
+    function createVariables(){
+        var airlines = [];
+        var departures = [];
+        var returns = [];
+        var prices = [];
+        var destinations = [];
+
+    for (var i = 0; i < flightResults.length; i++){
+        airlines[i] = flightResults[i].airline
+        departures[i] = flightResults[i].departure
+        returns[i] = flightResults[i].return
+        prices[i] = flightResults[i].price
+        destinations[i] = flightResults[i].destination
+        }
+    console.log(airlines)
+    console.log(departures)
+    console.log(returns)
+    console.log(prices)
+    console.log(destinations)
+    }
+    
+
+    createVariables();
+
+    // var city = {
+    //     search(cityCode){
+    
+    //         var cityQueryURL = 'https://api.sandbox.amadeus.com/v1.2/location/' + cityCode + '?apikey=wMaXQOdEowMqQ7QJtcHGjjt67AxtGJ6K';
+    
+    //         $.ajax({
+    //             url: cityQueryURL,
+    //             method: 'GET',
+    //         }).then(function(response){
+    //             console.log(response)
+    //             if(response.city.state === ''){
+    //                 var city = (response.city.name + ', ' + response.city.country)
+    //             }else {
+    //                 var city = (response.city.name + ', ' + response.city.state)
+    //             }
+    //             console.log(city)
+    
+    //             cities[i] = {
+    //                 'cityName': city,
+    //             }
+    //             console.log(cities[i])
+    
+    //             var cityResults = JSON.stringify(cities[i])
+    //             localStorage.setItem('cityResults', cityResults)
+    //             console.log(cityResults)
+    //         })
+    //     }
+    // };
+
+    // if(flightResults !== 'null'){
+    //     $('#resultDetails').show(10)
+    //     $('#flightDetails').show(10)
+    // }
 })
